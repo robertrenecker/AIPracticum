c3d040235edd5002540c711024b32fc09fcdd2a9
robert renecker <sylvan.rrenecker10@gmail.com>
2019-04-30 19:02:41 -0600

    model

--- ImageClassifier/ImageClassifier/ViewController.swift
+++ ImageClassifier/ImageClassifier/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let imageModel =
+        
     }
 
 
